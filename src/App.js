08fd3f99c983af5eb3b6bf114e2537e8da6fabb1
Junior Marques <junioralcant111@gmail.com>
2019-07-01 10:52:01 -0300

    04 Instalando styled-components e usando local e globalmente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+// import styled from 'styled-components';
+
+// STYLE COMPONENT LOCAL
+
+// const Title = styled.h1`
+//   color: #f00;
+//   font-size: 32px;
+// `;
+
+const App = () => (
+  // Style Component Global
+  <Fragment>
+    <GlobalStyle />
+    <div className="App" />
+  </Fragment>
+
+  //   <div className="App">
+  //     <Title>Hello World</Title>
+  //   </div>
+);
 
 export default App;
