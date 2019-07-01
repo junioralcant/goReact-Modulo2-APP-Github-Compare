b918c3af766ec991f28ed512e6752e1596dcf67f
Junior Marques <junioralcant111@gmail.com>
2019-07-01 16:18:02 -0300

    08 Enviando repositórios com props

--- src/Components/CompareList/index.js
+++ src/Components/CompareList/index.js
@@ -2,94 +2,32 @@ import React from 'react';
 
 import { Container, Repository } from './style';
 
-const CompareList = () => (
+const CompareList = ({ repositores }) => (
   <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    
+    {repositores.map(repository => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count}
+            <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </Container>
 );
 
